939548aeadac643a17f480af6026ef052de936a3
kiptubei <kiptubei@gmail.com>
2020-08-10 17:52:25 +0300

    Added edit form functions edit and update in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+  @user.update(user_params)
+
+  flash.notice = "User '#{@user.username}' Updated!"
+  render :new
+end
+
+
   private
   def user_params
   params.require(:user).permit(:username, :email, :password)
